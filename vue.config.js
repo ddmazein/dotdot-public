07321875f1c3a7aa896a49c9826b2a8d547f201d
Pavel Kolyada <pavelkolyada1@gmail.com>
2023-10-11 17:16:36 +0300

    TD-101 add prop for svg in icon components

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false,
+  lintOnSave: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src/components/"),
+      },
+    },
+  },
 });
